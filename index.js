99f59fdf92fc2fdf446830daf43cda4e1a289cb0
Usheninte <jrandangana@gmail.com>
2018-06-28 02:53:39 +0100

    simplified vanilla JS code

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ var button = document.querySelector('button');
 var body = document.querySelector('body');
 
 function OnOff() {
-	if (body.style.background != "black") {
-		body.style.background = "black";
-	}
-	else {
-		body.style.background = "white";
-	};
+	body.classList.toggle('switch');
 };
 
-button.addEventListener("click", OnOff);
+button.addEventListener('click', OnOff);
