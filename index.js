120c75b3328465898b2f5043d0868dc2231aa6bc
Usheninte <jrandangana@gmail.com>
2019-01-16 15:05:29 +0100

    edited lightSwitch

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-let 
+let button = document.querySelector('button');
+let body = document.querySelector('body');
+let copyr = document.querySelector('#copyr');
+
+function OnOff() {
+	let body.style.backgroundColor = '#212121';
+	let 
+
+	body.classList.toggle('');
+};
+
+button.addEventListener('click', OnOff);
