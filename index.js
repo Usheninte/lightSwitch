44a6f9bb9a5a47946404d89f2ada1ad922646f60
Usheninte <jrandangana@gmail.com>
2019-01-16 15:02:02 +0100

    edited lightSwitch

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var button = document.querySelector('button');
-var body = document.querySelector('body');
-
-function OnOff() {
-	body.classList.toggle('switch');
-};
-
-button.addEventListener('click', OnOff);
+let 
